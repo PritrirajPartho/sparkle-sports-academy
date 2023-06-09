bb05754b402ca296724683526d46bdc6b37455f0
PritrirajPartho <dapartho5616@gmail.com>
2023-06-09 14:08:57 +0600

    jwt-axios-secured

--- src/hooks/useBooked.jsx
+++ src/hooks/useBooked.jsx
@@ -1,17 +1,20 @@
 import { useQuery } from '@tanstack/react-query'
-// import useAxiosSecure from './useAxiosSecure';
 import useAuth from './useAuth';
+import useAxiosSecure from './useAxiosSecure';
+
+
 const useBooked = () => {
     const { user, loading } = useAuth();
-    // const token = localStorage.getItem('access-token');
-    // const [axiosSecure] = useAxiosSecure();
+    const token = localStorage.getItem('access-token');
+    const [axiosSecure] = useAxiosSecure();
+
     const { refetch, data: booked = [] } = useQuery({
         queryKey: ['bookeds', user?.email],
         enabled: !loading,
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/bookeds?email=${user?.email}`)
+            const res = await  axiosSecure(`/bookeds?email=${user?.email}`)
             console.log('res from axios', res)
-            return res.json();
+            return res.data;
         },
     })
 
