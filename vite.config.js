a9ba1acdcbb6dcf92d91e28b97825258ec5221cf
unknown <dpartho5616@gmail.com>
2024-06-03 10:37:59 +0600

    latest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config.js
 export default defineConfig({
   plugins: [react()],
 })
